3173bbf7bd4a8079fdafcc9a3b93d6938c8169cd
Joel Niklaus <joel.niklaus@thomsonreuters.com>
2022-06-02 17:45:19 +0200

    updated prepare_data to include larger posture50k dataset

--- examples/language_modeling/prepare_data.py
+++ examples/language_modeling/prepare_data.py
@@ -12,15 +12,38 @@ def prepare_data(base_folder, overwrite_cache=False, debug=False):
         return
 
     Path(folder).mkdir(parents=True, exist_ok=True)
-    dataset = load_dataset("cuad")
 
+    prepare_cuad(debug, folder)
+    prepare_posture50k(debug, folder)
+
+
+def prepare_posture50k(debug, folder):
+    splits = ["train", "dev", "test"]
+    posture50k = load_dataset('json', data_files={split: f"TRProceduralPosture50K/opinion_{split}.txt"
+                                                  for split in splits})
+    for split in splits:
+        rows = posture50k[split]['sections']
+        if debug:
+            rows = rows[:100]
+        output_file = "test" if split == "test" else "train"  # save everything else to train
+        with open(f"data/{output_file}.txt", 'a+') as output:
+            for row in rows:
+                text = ""
+                for section in row:
+                    text += section["headtext"] + "\n".join(section["paragraphs"]) + "\n"
+                    text = re.sub(r'\n+', '\n', text).strip()
+                    output.write(str(text) + '\n')
+        print(f"Saved the data to the folder {folder}")
+
+
+def prepare_cuad(debug, folder):
+    dataset = load_dataset("cuad")
     for split in ['train', 'test']:
         text = dataset[split]['context']
         if debug:
             text = text[:100]
-
         with open(f"data/{split}.txt", 'w+') as output:
             for row in text:
                 row = re.sub(r'\n+', '\n', row).strip()
                 output.write(str(row) + '\n')
-    print(f"Saved the data to the folder {folder}")
+        print(f"Saved the data to the folder {folder}")
